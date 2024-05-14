c6325ca62499583d3d66dd35c73c32057031d8e4
dugksl23 <dugksl23@gmail.com>
2024-05-14 11:32:34 +0900

    jpa와 mapper 실용적 사용

--- src/main/java/hello/itemservice/config/MemoryConfig.java
+++ src/main/java/hello/itemservice/config/MemoryConfig.java
@@ -10,10 +10,10 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class MemoryConfig {
 
-    @Bean
-    public ItemService itemService() {
-        return new ItemServiceV1(itemRepository());
-    }
+//    @Bean
+//    public ItemService itemService() {
+//        return new ItemServiceV1(itemRepository());
+//    }
 
 //    @Bean
     public ItemRepository itemRepository() {
--- src/main/java/hello/itemservice/service/ItemServiceV1.java
+++ src/main/java/hello/itemservice/service/ItemServiceV1.java
@@ -4,6 +4,7 @@ import hello.itemservice.domain.Item;
 import hello.itemservice.repository.ItemRepository;
 import hello.itemservice.repository.ItemSearchCond;
 import hello.itemservice.repository.ItemUpdateDto;
+import hello.itemservice.repository.jdbcTemplate.ItemJdbcRepository;
 import hello.itemservice.repository.jpa.ItemJpaRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -17,7 +18,9 @@ import java.util.Optional;
 @Transactional(readOnly = true)
 public class ItemServiceV1 implements ItemService {
 
+    // 실용적인 구조, Mapper 와 JPA 모두 의존.
     private final ItemRepository itemRepository;
+    private final ItemJdbcRepository itemJdbcRepository;
 
     @Override
     @Transactional
